805e7ad83d29e8280fc24ad8d2b7cbd7d3e2edae
Thong Phan <quang.thong2001@gmail.com>
2025-07-06 21:28:45 +0200

    remove build

--- micro_speech_openamp/build/modules/libmetal/libmetal/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/thongp/zephyrproject/modules/hal/libmetal/libmetal
-# Build directory: /home/thongp/zephyrproject/zephyr/samples/subsys/ipc/zephyr-application/micro_speech_openamp/build/modules/libmetal/libmetal
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
--- micro_speech_openamp/build/modules/open-amp/open-amp/lib/rpmsg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/thongp/zephyrproject/modules/lib/open-amp/open-amp/lib/rpmsg
-# Build directory: /home/thongp/zephyrproject/zephyr/samples/subsys/ipc/zephyr-application/micro_speech_openamp/build/modules/open-amp/open-amp/lib/rpmsg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
