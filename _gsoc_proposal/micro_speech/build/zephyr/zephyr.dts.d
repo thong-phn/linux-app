105ac24c33f59112b585dddcaaf62ee041db707b
Thong Phan <mrthonglion@gmail.com>
2025-09-07 18:53:03 +0000

    remove build folder

--- _gsoc_proposal/micro_speech/build/zephyr/zephyr.dts.d
+++
@@ -1,4 +0,0 @@
-empty_file.o: /home/thongp/zephyrproject/zephyr/misc/empty_file.c \
- /home/thongp/zephyrproject/zephyr/boards/qemu/xtensa/qemu_xtensa_dc233c.dts \
- /home/thongp/zephyrproject/zephyr/dts/xtensa/dc233c.dtsi \
- /home/thongp/zephyrproject/zephyr/dts/common/skeleton.dtsi
